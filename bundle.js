51eb6b68f891ed7c1985d5c6b6e90973fcdc89cf
Guilherme Carvalho <carvalhogsc@gmail.com>
2020-03-14 21:15:15 -0300

    Resolução do Exercicio 3

--- bundle.js
+++ bundle.js
@@ -1,35 +1,42 @@
 "use strict";
 
-var usuarios = [{
+// 3.1
+var arr = [1, 2, 3, 4, 5];
+var arrConvertido = arr.map(function (item) {
+  return item + 10;
+});
+console.log(arrConvertido); //3.2
+
+var usuario = {
   nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}];
-var idades = usuarios.map(function (_ref) {
-  var idade = _ref.idade;
-  return idade;
-});
-console.log(idades);
-var filtrarUsuarios = usuarios.filter(function (_ref2) {
-  var idade = _ref2.idade,
-      empresa = _ref2.empresa;
-  return idade > 18 && empresa === "Rocketseat";
-});
-console.log(filtrarUsuarios);
-var procurarUsuarioEmpresa = usuarios.find(function (_ref3) {
-  var empresa = _ref3.empresa;
-  return empresa === "Google";
-});
-console.log(procurarUsuarioEmpresa);
-var mapaFiltro = usuarios.filter(function (item) {
-  return item.idade * 2 < 50;
-});
-console.log(mapaFiltro);
+  idade: 23
+};
+
+var mostraIdade = function mostraIdade(usuario) {
+  return usuario.idade;
+};
+
+console.log(mostraIdade(usuario)); //3.3
+
+var nome = "Diego";
+var idade = 23;
+
+var mostraUsuario = function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome)); //3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
+
+console.log(promise());
